e11594fcafaff43b4b44e714ea7fc9748ed2e1fe
arun2604 <54104684+arun2604@users.noreply.github.com>
2023-05-08 01:24:00 +0530

    Add files via upload

---
+++ client-react/src/chatsPage.js
@@ -0,0 +1,16 @@
+import { PrettyChatWindow } from "react-chat-engine-pretty";
+
+const ChatsPage = (props) => {
+  return (
+    <div style={{ height: "100vh", width: "100vw" }}>
+      <PrettyChatWindow
+        projectId={"6b8e8de2-186b-4d9b-bd4f-f5055f2b6902"}
+        username={props.user.username} // adam
+        secret={props.user.secret} // pass1234
+        style={{ height: "100%" }}
+      />
+    </div>
+  );
+};
+
+export default ChatsPage;
